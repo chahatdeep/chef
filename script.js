2ad0e8258e28e36722223722c93693260c93eb09
Chahat Deep Singh <chahat@umd.edu>
2025-05-04 12:16:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
       "Indian/Aam Ka Achaar (Home).md",
       "Indian/Aam Ka Achaar (Alternative).md",
       "Indian/Dal Makhani.md"
+      "Indian/Sankhla Kulfi.md"
     ],
     Italian: [
       "Italian/Spaghetti Aglio e Olio.md"
